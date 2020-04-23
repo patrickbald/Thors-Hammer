64b1328f3163f96068fac7d69c9097530b6b464f
Patrick Bald <pbald@nd.edu>
2020-04-22 22:06:29 -0400

    socket_listen initial work

--- src/socket.c
+++ src/socket.c
@@ -19,19 +19,60 @@
  * @return  Allocated server socket file descriptor.
  **/
 int socket_listen(const char *port) {
+
     /* Lookup server address information */
+    struct addrinfo hints = {
+        .ai_family =    AF_UNSPEC,      // IPv4 and IPv6
+        .ai_socktype =  SOCK_STREAM,    // TCP
+        .ai_flags =     AI_PASSIVE,     // use all interfaces
+    };
+
+    struct addrinfo *results;
+
+    int status;
+    // host is null bc looking at itself
+    if((status = getaddrinfo(NULL, port, &hints, &results)) != 0){
+        fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(status));
+        return -1;
+    }
+
 
     /* For each server entry, allocate socket and try to connect */
     int socket_fd = -1;
     for (struct addrinfo *p = results; p != NULL && socket_fd < 0; p = p->ai_next) {
+
 	/* Allocate socket */
+
+        if((socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0){
+            fprintf(stderr, "Unable to make socket: %s\n", strerror(errno));
+            continue;
+        }
 
 	/* Bind socket */
 
+        if(bind(server_fd, p->ai_addr, p->ai_addrlen) < 0){
+            fprintf(stderr, "Unable to bind: %s\n", strerror(errno));
+            close(server_fd);
+            server_fd = -1;
+            continue;
+        }
+
     	/* Listen to socket */
+
+        // SOMAXCONN limits number of connections in the connection queue
+        if(listen(server_fd, SOMAXCONN) < 0){
+            fprintf(stderr, "Unable to listen: %s\n", strerror(errno));
+            close(server_fd);
+            server_fd = -1;
+            continue;
+        }
+
     }
 
+    // Release allocted address information
+
     freeaddrinfo(results);
+
     return socket_fd;
 }
 
