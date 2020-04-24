75507f7a3aed85c50ff5051ba36d9a8c1cf9998f
Patrick Bald <pbald@nd.edu>
2020-04-23 20:02:30 -0400

    single.c complete

--- src/single.c
+++ src/single.c
@@ -18,9 +18,19 @@ int single_server(int sfd) {
     while (true) {
     	/* Accept request */
 
+        Request *request = accept_request(sfd);
+        if(!request){
+            log("Unable to accept request: %s\n", strerror(errno));
+            continue;
+        }
+
 	/* Handle request */
+        handle_request(request);
+
 
 	/* Free request */
+        free_request(request);
+
     }
 
     /* Close server socket */
